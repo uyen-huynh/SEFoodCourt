5e866827271e1b73ca4ca2dde7bb25fb939d58e6
Dang Ngoc Tam <dangngoctam00@gmail.com>
2020-06-23 22:43:03 +0700

    Add manager view-controller-model and related data

---
+++ SFC/Controllers/ManagerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SFC.Models;
+using System.IO;
+//using MySql.Data.MySqlClient;
+namespace SFC.Controllers
+{
+    public class ManagerController : Controller
+    {
+        //Food _food;
+        //public ManagerController(Food food)
+        //{
+        //    _food = food;
+        //}
+        // GET: Manager                
+        public ActionResult HomeManager()
+        {
+            List<food> listFood = new List<food>();
+            using (DBModel dbModel = new DBModel())
+            {
+                listFood = dbModel.food.ToList<food>();
+            }           
+            return View(listFood);
+        }
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(food food)
+        {
+            if (ModelState.IsValid)
+            {
+                using(DBModel dbModel = new DBModel())
+                {
+                    dbModel.food.Add(food);
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("HomeManager");
+            }
+            return RedirectToAction("HomeManager");
+            //return View(food);            
+        }
+    }
+}
