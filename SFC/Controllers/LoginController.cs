14c147dac0659faba7e98c0812de1e9ed8f25b50
nhat12082000@gmail.com <nhat12082000@gmail.com>
2020-07-10 14:14:02 +0700

    Customize Login Page, Add Function LoginController, Add Class Account and AccountService.

--- SFC/Controllers/LoginController.cs
+++ SFC/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SFC.Models;
 
 namespace SFC.Controllers
 {
@@ -13,7 +14,6 @@ namespace SFC.Controllers
         {
             return View();
         }
-
         public ActionResult Manager()
         {
             return View();
@@ -23,5 +23,25 @@ namespace SFC.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public JsonResult CheckValidUser(string username, string password)
+        {
+            string result = "Success";
+            CustomerAccount cur = new CustomerAccount();
+            try
+            {
+                cur = AccountService.CheckLogin(username, password);
+            }
+            catch (Exception e)
+            {
+                if (e.Message != null)
+                    result = e.Message;
+            }
+            ViewBag.currentAccount = cur;
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
---
+++ SFC/Models/CustomerAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+
+namespace SFC.Models
+{
+    public class CustomerAccount
+    {
+        public string username { get; set; }
+        public string cipherPassword { get; set; }
+        public string name { get; set; }
+        public int birthYear { get; set; }
+        public string email { get; set; }
+
+        public CustomerAccount() { }
+        public CustomerAccount(string username, string plainPassword, string name, int birthYear, string email)
+        {
+            this.username = username;
+            cipherPassword = AccountService.ComputeMD5Hash(plainPassword);
+            this.name = name;
+            this.birthYear = birthYear;
+            this.email = email;
+        }
+        public CustomerAccount(CustomerAccount src)
+        {
+            username = src.username;
+            cipherPassword = src.cipherPassword;
+            name = src.name;
+            birthYear = src.birthYear;
+            email = src.email;
+        }
+
+    }
+}
