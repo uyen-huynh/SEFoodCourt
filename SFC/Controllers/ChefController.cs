b226a670f937680508c4f71043d828bc206654c4
Trong Nghia Tran <52828588+nghiatran1792000@users.noreply.github.com>
2020-07-12 02:25:50 +0700

    upload controller for chef

---
+++ SFC/Controllers/ChefController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFC.Controllers
+{
+    public class ChefController : Controller
+    {
+        // GET: Chef
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Complete()
+        {
+            return View();
+        }
+    }
+}
